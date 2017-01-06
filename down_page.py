240c1f8aadf78ef870687cf4adcd342fbbd71428
ludovitsiket <siket.ludovit@gmail.com>
2017-01-06 13:24:11 +0100

    odstranena funkcia make_file()

--- down_page.py
+++ down_page.py
@@ -7,14 +7,10 @@ def main():
     download(url,content_file)
 
 def download(url,content_file):
-    make_file(content_file)
     html=urllib.request.urlopen(url)
     read_page=html.read()
     f=open(content_file, "w")
     f.write(str(read_page))
     f.close()
-
-def make_file(content_file):
-    subprocess.call( ['touch', content_file ] )
 
 main()
