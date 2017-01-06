91be7a5d3447f1de6eb36521bbf86f0466c298f9
ludovitsiket <siket.ludovit@gmail.com>
2017-01-06 13:01:24 +0100

    pridane funkcie make_file() a download()
    make_file() vytvara prazdny subor do ktoreho funkcia download() uklada
    obsah web stranky

--- down_page.py
+++ down_page.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
-from urllib.request import urlopen
-import sys
+import urllib.request, sys,subprocess
 
 def main():
     url=sys.argv[1]
-    print(url)
-    return
+    content_file="file.txt"
+    download(url,content_file)
+
+def download(url,content_file):
+    make_file(content_file)
+    html=urllib.request.urlopen(url)
+    read_page=html.read()
+    f=open(content_file, "w")
+    f.write(str(read_page))
+    f.close()
+
+def make_file(content_file):
+    subprocess.call( ['touch', content_file ] )
 
 main()
