2aa17aa51ea97db7fe75cabfe056e680e5d3a7a5
ludovitsiket <siket.ludovit@gmail.com>
2017-01-10 19:32:48 +0100

    Pridana funkcia write_page()
    Funguje bez problemov.

--- down_page.py
+++ down_page.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-import urllib.request, sys, re
+import urllib.request, sys, re,os
 
 def main():
     content_file="page.html"
     directory=sys.argv[2]
     try:
         url=sys.argv[1]
-    except IndexError:
-        print("""Syntax: python3 down_page.py "http://www.name_of_page.com" "local_directory" """)
+    except IndexError as e:
+        print(e,"""Syntax: python3 down_page.py "http://www.name_of_page.com" "local_directory" """)
         sys.exit()
     try:
         download(url,content_file, directory)
@@ -21,19 +21,20 @@ def download(url,content_file, directory):
         src = page.read().decode('utf8')
     except UnicodeDecodeError:
         src = page.read()
-    f=open(content_file, "w")
-    f.write(str(src))
-    f.close()
+    write_page(src, content_file,directory)
+
+def write_page(src, content_file,directory):
+    with open(content_file) as local_page:
+        local_page.write(str(src))
     download_image(url,directory,src)
 
 def download_image(url,directory,page):    
     images=re.findall('img .*?src="(.*?)"',page)
     images.sort()
-    urllib.request.urlcleanup()
     for image in images:
         url=page+image
         try:
-            pic_name=directory+image[7:]  # OK
+            pic_name=directory+image[7:] 
             with open(pic_name, "wb") as local_file:
                 urllib.request.retrieve(url, local_file)
         except:
