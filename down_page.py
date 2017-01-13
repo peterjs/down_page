4f2f17bc6c1db7a71cd27fcf9fe855f676ff7104
ludovitsiket <siket.ludovit@gmail.com>
2017-01-13 18:13:52 +0100

    Nova funkcia find_images_on_page()

--- down_page.py
+++ down_page.py
@@ -27,8 +27,12 @@ def write_web_page_content_to_local_file(data_from_web_page, local_web_page):
         local_page.write(data_from_web_page)
     print("Stranka stiahnuta.")
 
+def find_images_on_page(data_from_web_page):
+    img=re.findall('img .*?src="(.*?)"',data_from_web_page)
+    return img
+
 def download_images_from_web_page(directory_to_download, data_from_web_page):    
-    images_on_web_page=re.findall('img .*?src="(.*?)"',data_from_web_page)
+    images_on_web_page=find_images_on_page(data_from_web_page)
     print("Stahujem obrazky. Cakajte prosim.")
     for image in images_on_web_page:
         local_picture=os.path.join(directory_to_download,os.path.basename(image))
