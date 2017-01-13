4365f29e5d70d7ba48bc6c43dbf0ab3a2bc1128a
ludovitsiket <siket.ludovit@gmail.com>
2017-01-13 17:52:21 +0100

    Funkcia pre stahovanie rozdelena na 3 funkcie
    Branch nesiel pushnut, preto som to spravil takto na tvrdo.

--- down_page.py
+++ down_page.py
@@ -6,21 +6,26 @@ def main():
         local_web_page="page.html"
         web_page_url=sys.argv[1]
         directory_to_download=sys.argv[2]
-        download(web_page_url, local_web_page, directory_to_download)
+        data_from_web_page = download_web_page_data(web_page_url, local_web_page, directory_to_download)
+        write_web_page_content_to_local_file(data_from_web_page, local_web_page)
+        download_images_from_web_page(directory_to_download, data_from_web_page)
     except:
            print("""Syntax: python3 down_page.py "http://www.name_of_page.com" "local_directory_to_download" """)
 
-def download(web_page_url, local_web_page, directory_to_download):
+def download_web_page_data(web_page_url, local_web_page, directory_to_download):
     request_to_page = urllib.request.Request(web_page_url)
     web_page = urllib.request.urlopen(request_to_page)
     try:
         data_from_web_page= web_page.read().decode('utf8')
+        return data_from_web_page
     except UnicodeDecodeError:
         data_from_web_page = web_page.read()
+        return data_from_web_page
+
+def write_web_page_content_to_local_file(data_from_web_page, local_web_page):
     with open(local_web_page,"w") as local_page:
         local_page.write(data_from_web_page)
     print("Stranka stiahnuta.")
-    download_images_from_web_page(directory_to_download, data_from_web_page)
 
 def download_images_from_web_page(directory_to_download, data_from_web_page):    
     images_on_web_page=re.findall('img .*?src="(.*?)"',data_from_web_page)
