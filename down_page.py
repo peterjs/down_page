ed216eac00d8af63bfc86df057d326836d99263b
ludovitsiket <siket.ludovit@gmail.com>
2017-01-14 12:00:06 +0100

    Sprehladnene definicie funkcii.

--- down_page.py
+++ down_page.py
@@ -12,35 +12,35 @@ def main():
     except:
            print("""Syntax: python3 down_page.py "http://www.name_of_page.com" "local_directory_to_download" """)
 
-def open_web_page(web_page_url):
-    request_to_page = urllib.request.Request(web_page_url)
+def open_web_page(url):
+    request_to_page = urllib.request.Request(url)
     page = urllib.request.urlopen(request_to_page)
     return page
 
-def download_web_page_data(web_page_url):
-    web_page = open_web_page(web_page_url) 
+def download_web_page_data(url):
+    content= open_web_page(url) 
     try:
-        return web_page.read().decode('utf8')
+        return content.read().decode('utf8')
     except UnicodeDecodeError:
-        return web_page.read()
+        return content.read()
 
-def write_web_page_content_to_local_file(data_from_web_page, local_web_page):
+def write_web_page_content_to_local_file(data, destination):
     print("Stahujem stranku.")
-    with open(local_web_page,"w") as local_page:
-        local_page.write(data_from_web_page)
+    with open( destination,"w") as local_file:
+        local_file.write(data)
     print("Stranka stiahnuta.")
 
-def find_images_on_page(data_from_web_page):
-    img=re.findall('img .*?src="(.*?)"',data_from_web_page)
+def find_images_on_page(data):
+    img=re.findall('img .*?src="(.*?)"',data)
     return img
 
-def download_images_from_web_page(directory_to_download, data_from_web_page):    
-    images_on_web_page=find_images_on_page(data_from_web_page)
+def download_images_from_web_page(directory, data_from_web_page):    
+    images=find_images_on_page(data_from_web_page)
     print("Stahujem obrazky. Cakajte prosim.")
-    for image in images_on_web_page:
-        local_picture=os.path.join(directory_to_download,os.path.basename(image))
+    for image in images:
+        picture_name=os.path.join(directory,os.path.basename(image))
         try:
-            urllib.request.urlretrieve(image, local_picture)
+            urllib.request.urlretrieve(image, picture_name)
         except (ValueError, urllib.error.URLError):
             pass
     print("Stahovanie dokoncene.")
