aa348383fae6e5e46af411c5206e4f8dc85c8db6
ludovitsiket <siket.ludovit@gmail.com>
2017-01-23 08:50:35 +0100

    Z name.rename odstranene premenovanie bodky
    Stratila sa tym pripona suboru.

--- down_page.py
+++ down_page.py
@@ -50,7 +50,6 @@ def join_path(directory, output_file):
 def create_file_name(directory, picture):
     name=join_path(directory, picture)
     name=name.replace("/","")
-    name=name.replace(".","_")
     name=name.replace(":","")
     name=name.replace("~","")
     name=os.path.join(directory, name)
